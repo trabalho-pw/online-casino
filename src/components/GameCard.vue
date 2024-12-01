a25a909df0c932df788af5a8d8a33e371f4515ac
lfguerra1 <luizfelipeguerra09@gmail.com>
2024-12-01 11:33:58 -0300

    Criando componente GameCard

---
+++ src/components/GameCard.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="game-card">
+      <div class="title">{{ title }}</div>
+      <div class="image-container">
+        <img :src="imageSrc" alt="Imagem do Jogo" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "GameCard",
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+      imageSrc: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .game-card {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #ddd; 
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 10px;
+    width: 300px; 
+  }
+  
+  .title {
+    font-size: 16px;
+    font-weight: bold;
+  }
+  
+  .image-container {
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+  }
+  
+  .image-container img {
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: cover;
+  }
+  </style>
+  
---
+++ src/views/GameSelectorView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <GameCard
+        title="Título Jogo 1"
+        imageSrc="caminho-para-imagem-1.jpg"
+      />
+      <GameCard
+        title="Título Jogo 2"
+        imageSrc="caminho-para-imagem-2.jpg"
+      />
+    </div>
+  </template>
+  
+  <script>
+  import GameCard from "../components/GameCard.vue";
+  
+  export default {
+    name: "GameSelectorView",
+    components: {
+      GameCard,
+    },
+  };
+  </script>
+  
