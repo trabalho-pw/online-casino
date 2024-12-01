f7837f40de4ae32ea9d17e52f50cda9e8891e486
lfguerra1 <luizfelipeguerra09@gmail.com>
2024-12-01 19:30:27 -0300

    centralized

--- src/views/GameSelectorView.vue
+++ src/views/GameSelectorView.vue
@@ -1,22 +1,63 @@
 <template>
-    <div>
-      <GameCard 
-        title="Jogo Fantástico" 
-        imageSrc="https://example.com/imagem.jpg" 
-        description="Este é um jogo incrível onde você pode explorar mundos fantásticos e aventuras emocionantes." 
-        gameUrl="https://example.com/jogo"
-      />
-    </div>
-  </template>
-  
-  <script>
-  import GameCard from "../components/GameCard.vue";
-  
-  export default {
-    name: "GameSelectorView",
-    components: {
-      GameCard,
-    },
-  };
-  </script>
+  <div class="game-cards-container">
+    <GameCard
+      v-for="(game, index) in games"
+      :key="index"
+      :title="game.title"
+      :imageSrc="game.imageSrc"
+      :description="game.description"
+      :gameUrl="game.gameUrl"
+    />
+  </div>
+</template>
+
+<script>
+import GameCard from "@/components/GameCard.vue";
+
+export default {
+  name: "GameCardsView",
+  components: {
+    GameCard,
+  },
+  data() {
+    return {
+      games: [
+        {
+          title: "Jogo 1",
+          imageSrc: "",
+          description: "Descrição do Jogo 1",
+          gameUrl: "#",
+        },
+        {
+          title: "Jogo 2",
+          imageSrc: "",
+          description: "Descrição do Jogo 2",
+          gameUrl: "#",
+        },
+        {
+          title: "Jogo 3",
+          imageSrc: "",
+          description: "Descrição do Jogo 3",
+          gameUrl: "#",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.game-cards-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;  
+  gap: 10px; 
+  padding: 10px;
+  width: 100%;  
+}
+
+.game-card:first-child {
+  margin-top: 0; 
+}
+</style>
   
