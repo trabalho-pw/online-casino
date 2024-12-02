635115d754532106d9d3e181db3d79de00d25aa0
Alexsandro <jalexsandro2005@gmail.com>
2024-12-02 14:43:42 -0300

    criado modal de daily gift

---
+++ src/components/DailyGiftModal.vue
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<template>
+    <div class="modal-container">
+        <div class="modal">
+            <h1>Presente Diário</h1>
+            <img src="../assets/gift.svg" alt="imagem de presente">
+            <p>você recebeu 200 moedas</p>
+            <p>Volte amanhã para mais recompensas!</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .modal-container {
+        height: 100dvh;
+        width: 100vw;
+        background: #223f4a44;
+        z-index: 2;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .modal img {
+        width: 3em;
+    }
+</style>
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC7cxVZyNMLGJgZAV9VXjEnW5o3Yohf04Y",
-  authDomain: "online-casino-2ef89.firebaseapp.com",
-  projectId: "online-casino-2ef89",
-  storageBucket: "online-casino-2ef89.firebasestorage.app",
-  messagingSenderId: "989139214113",
-  appId: "1:989139214113:web:9b1bbd4ff1441d5c2ca860"
-};
+  apiKey: 'AIzaSyC7cxVZyNMLGJgZAV9VXjEnW5o3Yohf04Y',
+  authDomain: 'online-casino-2ef89.firebaseapp.com',
+  projectId: 'online-casino-2ef89',
+  storageBucket: 'online-casino-2ef89.firebasestorage.app',
+  messagingSenderId: '989139214113',
+  appId: '1:989139214113:web:9b1bbd4ff1441d5c2ca860',
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
 
-export { auth };
+export { auth }
