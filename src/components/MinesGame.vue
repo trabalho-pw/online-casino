f0713eca2e49c1d41140befc900e1b3dccde742f
marcospaulofjl <marcospaulofjl@gmail.com>
2024-12-02 23:39:39 -0300

    aposta funcionando

--- src/components/MinesGame.vue
+++ src/components/MinesGame.vue
@@ -1,188 +1,249 @@
 <template>
     <div>
+        <div class="cash">
+            <p>{{ cash }}</p>
+        </div>
+        <div class="game-board">
+            <div 
+                v-for="(square, index) in board" 
+                :key="index" 
+                class="square" 
+                :class="{ clicked: square.clicked, mine: isGameOver && square.mine }"
+                @click=" isBetPlaced && handleClick(index)"
+            >
+                <i v-if="square.clicked" :class="square.icon"></i>
+            </div>
+        </div>
         <div class="bet-input">
-            <input 
-                type="number" 
-                v-model.number="bet" 
-                placeholder="Insira sua aposta" 
-            />
-            <button @click="placeBet">Apostar</button>
+        <input 
+            type="number" 
+            v-model.number="bet" 
+            placeholder="Insira sua aposta" 
+        />
+        <button :disabled="!isBetValid" @click="placeBet">Começar o Jogo</button>
+        
         </div>
-
-        <div>
-            <div class="game-board">
-                <div 
-                    v-for="(square, index) in board" 
-                    :key="index" 
-                    class="square" 
-                    :class="{ clicked: square.clicked, mine: isGameOver && square.mine }"
-                    @click=" isBetPlaced && handleClick(index)"
-                >
-                    <i v-if="square.clicked" :class="square.icon"></i>
-                </div>
-            </div>
-            <div class="status">
-                <button @click="startGame">Começar Novo Jogo</button>
-            </div>
+        <div v-if="isBetPlaced" class="status">
+            <p>MULTIPLICADOR: {{ mult }}X</p>
+            <button :disabled="!isBetValid" @click="gameWin">RETIRAR: R${{win}}</button>
         </div>
+        
     </div>
 </template>
 
 <script>
 export default{
-    name: "MinesGame",
-    data() {
-        return {
-            board: [],         
-            mines: [],         
-            isGameOver: false, 
-            prize: 0,          
-            totalSquares: 25,  
-            bet: null,         
-            isBetPlaced: false, 
-        };
+name: "MinesGame",
+data() {
+    return {
+        board: [],         
+        mines: [],
+        isGameWin: false,         
+        isGameOver: false, 
+        mult: 1.0,          
+        totalSquares: 25,  
+        bet: null,         
+        isBetPlaced: false,
+        win: 0,
+        cash: 1000,
+    };
+},
+computed:{
+    isBetValid(){
+        return this.bet != null
     },
-    methods: {
+},
+methods: {
+    
+    startGame() {
+        this.isGameWin = false;
+        this.isGameOver = false;
+        this.board = [];
+        this.mines = [];
+        this.mult = 1.0;
+        this.win = this.bet;
         
-        startGame() {
-            this.isGameOver = false;
-            this.board = [];
-            this.mines = [];
-            this.prize = 0;
-            
-            for (let i = 0; i < this.totalSquares; i++) {
-                this.board.push({ clicked: false, mine: false, icon: "" });
+        
+        for (let i = 0; i < this.totalSquares; i++) {
+            this.board.push({ clicked: false, mine: false, icon: "" });
+        }
+
+        
+        this.placeMines();
+    },
+    placeMines() {
+        let mineCount = 5; 
+        while (this.mines.length < mineCount) {
+            const randomIndex = Math.floor(Math.random() * this.totalSquares);
+            if (!this.mines.includes(randomIndex)) {
+                this.mines.push(randomIndex);
             }
+        }
 
-            
-            this.placeMines();
-        },
-        placeMines() {
-            let mineCount = 5; 
-            while (this.mines.length < mineCount) {
-                const randomIndex = Math.floor(Math.random() * this.totalSquares);
-                if (!this.mines.includes(randomIndex)) {
-                    this.mines.push(randomIndex);
-                }
-            }
-
-            
-                this.mines.forEach(index => {
-                this.board[index].mine = true;
-                });
-        },
-        placeBet() {
-            if (this.bet > 0) {
-                this.isBetPlaced = true;
-                this.startGame();
-            } else {
-                alert("Por favor, insira um valor de aposta válido.");
-            }
-        },
-        handleClick(index) {
-            if (this.isGameOver || this.board[index].clicked) return;
-
-            const square = this.board[index];
-            square.clicked = true;
-
-            if (square.mine) {
-                this.gameOver();
-            } else {
-                this.prize += 10;
-                square.icon = "fa fa-gem"; 
-                if (this.prize >= 50) {
-                    this.isGameOver = true;
-                }
-            }
-        },
-        gameOver() {
-            this.isGameOver = true;
-
+        
             this.mines.forEach(index => {
-                const mineSquare = this.board[index];
-                mineSquare.icon = "fa fa-bomb"; 
-                mineSquare.clicked = true;
+            this.board[index].mine = true;
             });
+    },
+    placeBet() {
+        this.cash -= this.bet;
+        if (this.bet > 0) {
+            this.isBetPlaced = true;
+            this.startGame();
+        } else {
+            alert("Por favor, insira um valor de aposta válido.");
         }
     },
-    mounted() {
-        this.startGame();
+    handleClick(index) {
+        if (this.isGameWin || this.isGameOver || this.board[index].clicked) return;
+
+        const square = this.board[index];
+        square.clicked = true;
+
+        if (square.mine) {
+            this.gameOver();
+        } else {
+            this.mult += 0.5;
+            square.icon = "fa fa-gem";
+            this.win = this.bet * this.mult;
+            if (this.mult >= 3.5) {
+                this.isGameWin = true;
+            }
+        }
+    },
+    gameOver() {
+        this.isGameOver = true;
+        this.bet = null;
+
+        this.mines.forEach(index => {
+            const mineSquare = this.board[index];
+            mineSquare.icon = "fa fa-bomb"; 
+            mineSquare.clicked = true;
+        });
+    },
+    gameWin() {
+        this.cash += this.win; 
+        this.isGameWin = true;
+        this.bet = null;
+        
     }
+},
+mounted() {
+    this.startGame();
+}
 };
 
 </script>
 
 <style scoped>
+.status{
+font-family: "Bebas Neue";
+font-size: 1.5rem;
+gap: 10px;
+display: flex;
+justify-content: center;
+
+}
+p{
+border: 1px solid #ccc;
+width: 40%;
+border-radius: 10px;
+box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
+font-size: 1.5rem;
+padding: 7px;
+}
 .bet-input {
-    margin-bottom: 20px;
-    display: flex;
-    justify-content: center;
-    gap: 10px;
+margin-top: 20px;
+margin-bottom: 20px;
+display: flex;
+justify-content: center;
+gap: 10px;
 }
 
 input {
-    padding: 10px;
-    font-size: 16px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
+padding: 10px;
+font-size: 1.5rem;
+border: 1px solid #ccc;
+border-radius: 10px;
+height: 100%;
+width: 40%;
+font-family: "Bebas Neue", sans-serif;
+box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
 }
 
 .game-board {
-    display: grid;
-    grid-template-columns: repeat(5, 50px);
-    grid-template-rows: repeat(5, 50px);
-    gap: 10px;
-    justify-content: center;
-    
+display: grid;
+grid-template-columns: repeat(5, 50px);
+grid-template-rows: repeat(5, 50px);
+gap: 10px;
+justify-content: center;
+
 
 }
 
 .square {
-    width: 50px;
-    height: 50px;
-    background-color: #e0e0e0;
-    border-radius: 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 18px;
-    cursor: pointer;
-    transition: transform 0.6s ease, background-color 0.3s ease;
-    transform: rotateY(0);
-    
+width: 50px;
+height: 50px;
+background-color: #e0e0e0;
+border-radius: 5px;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 18px;
+cursor: pointer;
+transition: transform 0.6s ease, background-color 0.3s ease;
+transform: rotateY(0);
+
 }
 .square:hover {
-    background-color: #ccc;
+background-color: #ccc;
 }
 
 .square.clicked {
-    background-color: #a5d6a7;
-    transform: rotateY(180deg);
+background-color: #a5d6a7;
+transform: rotateY(180deg);
 }
 
 .square.mine {
-    background-color: #ec4c4c;
+background-color: #ec4c4c;
 }
 
 .status {
-    margin-top: 20px;
-    display: flex;
-    justify-content: center;
+margin-top: 20px;
+display: flex;
+justify-content: center;
 }
 
 button {
-    padding: 10px 20px;
-    background-color: #2196F3;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    font-size: 16px;
-    
+padding: 10px;
+width: 40%;
+color: white;
+background-color: #e63948;
+font-size: 1.5rem;
+font-weight: 500;
+font-family: "Bebas Neue", sans-serif;
+border: none;
+border-radius: 10px;
+transition: 0.2s ease;
+box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
+cursor: pointer;
+
 }
 
 button:hover {
-    background-color: #1976D2;
+scale: 1.03;
+cursor: pointer;
+}
+
+button:active{
+scale: 1;
+}
+
+button:disabled{
+background: #989898;
+transition: none;
+scale: 1;
+cursor: default;
 }
 
 </style>
